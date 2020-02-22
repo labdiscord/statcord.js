ebf7a7a30e672cd6e563e92b728c1e1180e5513f
Glitch (hello-express) <none>
2020-02-22 01:13:31 +0000

    🏗👶 Checkpoint ./src/main.js:697459/14

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ var request = require('snekfetch').post
 class StatCord {
   constructor(KEY = String, CLIENT = Object, sharding = false) {
     if(!KEY || typeof KEY != 'string') throw new Error("You have provided an item that is not a string. Please replace the item (statcord-api)")
-    if(!CLIENT || typeof CLIENT != 'object' || !CLIENT.USER) throw new Error("You have provided an item that is not a object. Please replace the item (statcord-api)")
-    this.baseURL = "https://statcord.com/apollo/post/status"
+    if(!CLIENT || typeof CLIENT != 'object' || !CLIENT.user) throw new Error("You have provided an item that is not a object. Please replace the item (statcord-api)")
+    this.baseURL = "https://statcord.com/apollo/post/stats"
     this.key = KEY;
     this.client = CLIENT;
     this.sharding = sharding
@@ -22,7 +22,7 @@ class StatCord {
     } else userSize = this.client.guilds.size    
     return await request(this.baseURL)
     .send({
-      id: this.client.id,
+      id: this.client.user.id,
       key: this.key,
       servers: guildSize,
       users: userSize
