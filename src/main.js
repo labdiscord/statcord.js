e588c7c661232d965bca2b2ee22294d04b3d5cab
Immortal Evil <52874725+ImmortalEvil@users.noreply.github.com>
2020-04-20 15:36:51 +0530

    Removed user input for sharding status and version
    I've removed the user input for the sharding status and ver12 to make the user's experience easier. Hope this helps...

--- src/main.js
+++ src/main.js
@@ -1,42 +1,76 @@
 var request = require('snekfetch').post
 class StatCord {
-  constructor(KEY, CLIENT, sharding = false, v12) {
+  constructor(KEY, CLIENT) {
     if(!KEY || typeof KEY != 'string') throw new Error("You have provided an item that is not a string. Please replace the item (statcord-api)")
     if(!CLIENT) throw new Error("You have provided an item that is not a object. Please replace the item (statcord-api)")
     this.baseURL = "https://statcord.com/apollo/post/stats"
     this.key = KEY;
     this.client = CLIENT;
-    this.sharding = sharding
-    this.ver12 = v12
   }
-  
-  async post() { 
+
+  async post() {
+let ver12;
+if(this.client.guilds.cache){
+ver12 = true
+} else {
+ver12 = false
+}
+let sharding;
+if(this.client.shard){
+sharding = true
+} else {
+sharding = false
+}
     var guildSize = 0;
-    if(this.sharding == true) {
-      var clientValues = await this.client.shard.fetchClientValues('guilds.size')
-      guildSize = clientValues.reduce((prev, guildCount) => prev + guildCount, 0)
-    } else {
-      if(this.ver12) {
-        guildSize = this.client.guilds.cache.size
-      } else guildSize = this.client.guilds.size
-    }
+  if(ver12) {
+if(sharding == true) {
+  await this.client.shard.fetchClientValues('guilds.cache.size')
+  	.then(results => {
+  		guildSize = results.reduce((prev, guildCount) => prev + guildCount, 0)
+  	})
+} else {
+guildSize = this.client.guilds.cache.size
+}
+} else {
+if(sharding == true) {
+  await this.client.shard.fetchClientValues('guilds.size')
+  	.then(results => {
+  		guildSize = results.reduce((prev, guildCount) => prev + guildCount, 0)
+  	})
+} else {
+guildSize = this.client.guilds.size
+}
+}
     var userSize = 0;
-    if(this.sharding == true) {
-      var clientValues = await this.client.shard.fetchClientValues('users.size')
-      userSize = clientValues.reduce((prev, guildCount) => prev + guildCount, 0)
-    } else {
-      if(this.ver12) {
-        userSize = this.client.guilds.cache.map(g => g.memberCount).reduce(function (accumulator, currentValue) {
-        return accumulator + currentValue;
-      }, 0);
-      } else userSize = this.client.guilds.map(g => g.memberCount).reduce(function (accumulator, currentValue) {
-        return accumulator + currentValue;
-      }, 0);
-    }
-    
+        if(ver12) {
+if(sharding == true) {
+  await this.client.shard.broadcastEval('this.guilds.cache.reduce((prev, guild) => prev + guild.memberCount, 0)')
+  	.then(results => {
+  		userSize = results.reduce((prev, memberCount) => prev + memberCount, 0)
+  	})
+  	.catch(console.error);
+} else {
+  userSize = this.client.guilds.cache.map(g => g.memberCount).reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+}
+} else {
+if(sharding == true) {
+  await this.client.shard.broadcastEval('this.guilds.reduce((prev, guild) => prev + guild.memberCount, 0)')
+  	.then(results => {
+  		userSize = results.reduce((prev, memberCount) => prev + memberCount, 0)
+  	})
+  	.catch(console.error);
+} else {
+  userSize = this.client.guilds.map(g => g.memberCount).reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue;
+  }, 0);
+}
+}
+
     return await request(this.baseURL, {
       headers: {
-       "Content-Type": "application/json" 
+       "Content-Type": "application/json"
       }
     })
     .send({
