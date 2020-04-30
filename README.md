0140a02cd562829c56785ff9c66a64f1c42f8462
Immortal Evil <52874725+ImmortalEvil@users.noreply.github.com>
2020-04-30 11:06:36 +0530

    Added v2 statcord
    Added posting commands function

--- README.md
+++ README.md
@@ -29,14 +29,39 @@ npm i statcord.js
 let Discord = require('discord.js')
 let client = new Discord.Client()
 let statcord = require('statcord.js')
-let statclient = new statcord("statcord.com-AddYourKeyHere", client)
- 
+
 client.on('ready', async () => {
+let statclient = new statcord("statcord.com-AddYourKeyHere", client)
   await statclient.autoPost()
 })
- 
+
 client.login("YourDiscordBotTokenHere")
 ```
+
+### Posting commands
+```js
+let Discord = require('discord.js')
+let client = new Discord.Client()
+let statcord = require('statcord.js')
+let statclient = new statcord("statcord.com-AddYourKeyHere", client)
+const prefix = 'YourPrefix'
+
+client.on('message', message => {
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command === 'ping'){
+    statclient.postCommand(command, message.author.id)
+    message.channel.send('Pong!')
+  }
+})
+
+client.login("YourDiscordBotTokenHere")
+```
+
+## Caution for Sharding Bots
+
+If you are using sharding in your bot, make sure you use the constructor (`let statclient = new statcord("statcord.com-AddYourKeyHere", client)`) for statClient only after all shards are spawned. If not, you will be resulted an error.
+
 
 ## Contributing
 
--- src/main.js
+++ src/main.js
@@ -1,110 +1,260 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 class Statcord {
-    constructor(KEY, CLIENT) {
-        if (!KEY || typeof KEY != 'string')
-            return console.error("You have provided an item that is not a string. Please replace the item (statcord-api)")
+  constructor(KEY, CLIENT) {
+    if (!KEY || typeof KEY != "string")
+      console.error(
+        "You have provided an item that is not a string. Please replace the item (statcord-api)"
+      );
 
-        if (!CLIENT)
-            return console.error("You have provided an item that is not a object. Please replace the item (statcord-api)")
+    if (!CLIENT)
+      console.error(
+        "You have provided an item that is not a object. Please replace the item (statcord-api)"
+      );
+    this.baseURL = "https://beta.statcord.com/mason/stats";
+    this.key = KEY;
+    this.client = CLIENT;
+    this.active = [];
+    this.commands = 0;
+    this.popular = [];
+  }
 
-        this.baseURL = "https://statcord.com/apollo/post/stats"
-        this.key = KEY;
-        this.client = CLIENT;
+  async post() {
+    if (require('discord.js').version >= '12.0.0') {
+      this.ver12 = true
+      console.log('You are in v12 discord.js')
+    } else {
+      this.ver12 = false
+      console.log('You are not in v12 discord.js')
     }
-
-    async post() {
-        let ver12;
-        if (this.client.guilds.cache) {
-            ver12 = true
-        } else {
-            ver12 = false
-        }
-        let sharding;
         if (this.client.shard) {
-            sharding = true
-        } else {
-            sharding = false
-        }
-        let guildSize = 0;
-        if (ver12) {
-            if (sharding == true) {
-                await this.client.shard.fetchClientValues('guilds.cache.size').then(results => {
-                    guildSize = results.reduce((prev, guildCount) => prev + guildCount, 0)
-                })
-            } else {
-                guildSize = this.client.guilds.cache.size
-            }
-        } 
-        else {
-            if (sharding == true) {
-                await this.client.shard.fetchClientValues('guilds.size').then(results => {
-                    guildSize = results.reduce((prev, guildCount) => prev + guildCount, 0)
-                })
-            } 
-            else {
-                guildSize = this.client.guilds.size
-            }
-        }
-        let userSize = 0;
-        if (ver12) {
-            if (sharding == true) {
-                await this.client.shard.broadcastEval('this.guilds.cache.reduce((prev, guild) => prev + guild.memberCount, 0)').then(results => {
-                    userSize = results.reduce((prev, memberCount) => prev + memberCount, 0)
-                }).catch(console.error);
-            } 
-            else {
-                userSize = this.client.guilds.cache.map(g => g.memberCount).reduce(function (accumulator, currentValue) {
-                    return accumulator + currentValue;
-                }, 0);
-            }
-        } 
-        else {
-            if (sharding == true) {
-                await this.client.shard.broadcastEval('this.guilds.reduce((prev, guild) => prev + guild.memberCount, 0)').then(results => {
-                    userSize = results.reduce((prev, memberCount) => prev + memberCount, 0)
-                }).catch(console.error);
-            }else {
-                userSize = this.client.guilds.map(g => g.memberCount).reduce(function (accumulator, currentValue) {
-                    return accumulator + currentValue;
-                }, 0);
-            }
-        }
-        
-        let body = {
-            "id": this.client.user.id,
-            "key": this.key,
-            "servers": guildSize,
-            "users": userSize
-        }
-        let response;
-  await fetch(this.baseURL, {
-                method: 'post',
-                body:    JSON.stringify(body),
-                headers: { 'Content-Type': 'application/json' },
-            }).then(res => {
-    response = {
-      message: res.statusText,
-      statusCode: res.status
-    }
-  }).catch(err => {
-   console.log(err)
-  })
-      if(response.statusCode === 200){
-        console.log(response)
+      if(this.ver12 === true){
+      if(this.client.shard.ids[0] !== 0) this.shard_id = true
       } else {
-        console.log(response)
+        if(this.client.shard.id !== 0) this.shard_id = true
+      }
+      this.sharding = true;
+    } else {
+      this.sharding = false;
+      this.shard_id = false
+    }
+    if(this.shard_id === true) return;
+    let guildSize = 0;
+    if (this.ver12 === true) {
+      if (this.sharding == true) {
+        await this.client.shard
+          .fetchClientValues("guilds.cache.size")
+          .then(results => {
+            guildSize = results.reduce(
+              (prev, guildCount) => prev + guildCount,
+              0
+            );
+          });
+      } else {
+        guildSize = this.client.guilds.cache.size;
+      }
+    } else {
+      if (this.sharding == true) {
+        await this.client.shard
+          .fetchClientValues("guilds.size")
+          .then(results => {
+            guildSize = results.reduce(
+              (prev, guildCount) => prev + guildCount,
+              0
+            );
+          });
+      } else {
+        guildSize = this.client.guilds.size;
       }
     }
-
-    async autoPost(){
-        console.log("Statcord Auto Post Started")
-        await this.post()
-        
-        setInterval(async function(arg1) {
-          console.log("POSTING")
-            await arg1.post()
-        }, 3600000,this)
+    let userSize = 0;
+    if (this.ver12 === true) {
+      if (this.sharding == true) {
+await this.client.shard.broadcastEval(`
+let array = []
+for(const data of this.guilds.cache.array()){
+for(const member of data.members.cache.array()){
+if(array.includes(member.id)) continue;
+array.push(member.id)
+}
+}
+array.length
+`).then(re => {
+for(const data of re){
+  userSize = userSize + data
+}
+}).catch(err => console.log(err))
+      } else {
+let array = []
+for(const data of this.client.guilds.cache.array()){
+  for(const member of data.members.cache.array()){
+if(array.includes(member.id)) continue;
+await array.push(member.id)
+}
+}
+        userSize = array.length
+      }
+    } else {
+      if (this.sharding == true) {
+await this.client.shard.broadcastEval(`
+let array = []
+for(const data of this.guilds.array()){
+for(const member of data.members.array()){
+if(array.includes(member.id)) continue;
+array.push(member.id)
+}
+}
+array.length
+`).then(re => {
+for(const data of re){
+  userSize = userSize + data
+}
+}).catch(err => console.log(err))
+      } else {
+let array = []
+for(const data of this.client.guilds.array()){
+  for(const member of data.members.array()){
+if(array.includes(member.id)) continue;
+await array.push(member.id)
+}
+}
+        userSize = array.length
+      }
     }
+    let popular = [];
+    let array = this.popular.sort(function(a, b) {
+      return b.count - a.count;
+    });
+    for (const data of array) {
+      if (array[0] === data || array[1] === data || array[2] === data) {
+        popular.push({
+          name: data.name,
+          count: data.count.toString()
+        });
+      } else continue;
+    }
+    let body = {
+      id: this.client.user.id,
+      key: this.key,
+      servers: guildSize.toString(),
+      users: userSize.toString(),
+      active: this.active.length.toString(),
+      commands: this.commands.toString(),
+      popular: popular
+    };
+    let response;
+    await fetch(this.baseURL, {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(res => {
+        response = {
+          message: res.statusText,
+          statusCode: res.status
+        };
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.active = [];
+    this.commands = 0;
+    this.popular = [];
+
+    if (response.statusCode === 200) {
+      //console.log(response);
+    } else {
+      console.log(response);
+    }
+  }
+
+  async autoPost() {
+    console.log("Statcord Auto Post Started");
+    await this.post();
+
+    setInterval(
+      async function(arg1) {
+        await arg1.post();
+      },
+      3600000,
+      this
+    );
+  }
+  async postCommand(command, author_id) {
+    if (require('discord.js').version >= '12.0.0') {
+      this.ver12 = true
+      console.log('You are in v12 discord.js')
+    } else {
+      this.ver12 = false
+      console.log('You are not in v12 discord.js')
+    }
+        if (this.client.shard) {
+      if(this.ver12 === true){
+      if(this.client.shard.ids[0] !== 0) this.shard_id = true
+      } else {
+        if(this.client.shard.id !== 0) this.shard_id = true
+      }
+      this.sharding = true;
+    } else {
+      this.sharding = false;
+      this.shard_id = false
+    }
+    if(this.sharding === false){
+    if (!command || typeof command != "string")
+      return console.error("You didn't provide enough parameters");
+
+    if (!author_id || typeof author_id != "string")
+      return console.error(
+        "You didn't provide enough parameters! Make sure the author id is a string!"
+      );
+console.log({command: command, author_id: author_id})
+    this.commands = this.commands + 1;
+    if (!this.active.includes(author_id)) this.active.push(author_id);
+    let obj = {
+      name: command,
+      count: 1
+    };
+    if (!this.popular.some(m => m.name === command)) {
+      this.popular.push(obj);
+    } else {
+      let fi = this.popular.find(m => m.name === command);
+      let objIndex = this.popular.findIndex(obj => obj.name == command);
+      this.popular[objIndex].count = fi.count + 1;
+    }
+    } else {
+      if(this.shard_id === false){
+            if (!command || typeof command != "string")
+      return console.error("You didn't provide enough parameters");
+
+    if (!author_id || typeof author_id != "string")
+      return console.error(
+        "You didn't provide enough parameters! Make sure the author id is a string!"
+      );
+console.log({command: command, author_id: author_id})
+    this.commands = this.commands + 1;
+    if (!this.active.includes(author_id)) this.active.push(author_id);
+    let obj = {
+      name: command,
+      count: 1
+    };
+    if (!this.popular.some(m => m.name === command)) {
+      this.popular.push(obj);
+    } else {
+      let fi = this.popular.find(m => m.name === command);
+      let objIndex = this.popular.findIndex(obj => obj.name == command);
+      this.popular[objIndex].count = fi.count + 1;
+    }
+      } else {
+      this.client.shard.broadcastEval(`
+let m = ${this.shard_id}
+if(m === true){
+const statcord = require('statcord.js')
+let client = new statcord(\`${this.key}\`, \`${this.client}\`)
+client.postCommand(\`${command}\`, \`${author_id}\`)
+}
+`)
+      }
+    }
+  }
 }
 
 module.exports = Statcord;
